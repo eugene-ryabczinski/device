6e055e69e6eeccf08ed7fd3ac88bd62668d10aa7
Евгений Р <eugene.ryabczinski@yandex.ru>
2019-02-23 04:17:54 +0300

    Начало автоматизации. Конфигурация автопрефиксера, минификация css

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,62 @@
-module.exports = function(grunt) {	
-	
+module.exports = function (grunt) {
+
 	const sass = require('node-sass');
 
 	grunt.initConfig({
-	    sass: {
-	        options: {
-	            implementation: sass,
-	            outputStyle: 'expanded',
-	            indentWidth: 4,
-	            sourceMap: false
-	        },
-	        dist: {
-	            files: {
-	                'css/style.css': 'sass/style.scss'
-	            }
-	        }
-	    },
-	    
-        watch: {
-            options: {
-                spawn: false
-            },
-            scripts: {
-                files: ['sass/*.scss'],
-                tasks: ['sass'] // пишем какие задачи будем запускать когда какой-либо файл scss изменён. наприемр так ['sass', 'postcss'] и т.д.
-            }
-        }
+		sass: {
+			options: {
+				implementation: sass,
+				outputStyle: 'expanded',
+				indentWidth: 4,
+				sourceMap: false
+			},
+			dist: {
+				files: {
+					'css/style.css': 'sass/style.scss'
+				}
+			}
+		},
+
+		postcss: {
+			options: {
+				map: false,
+				processors: [
+					require('autoprefixer')({ browsers: 'last 3 versions' })
+				]
+			},
+			dist: {
+				src: 'css/*.css'
+			}
+		},
+
+		csso: {
+			compress: {
+				options: {
+					report: 'gzip'
+				},
+				files: {
+					'css/style.min.css': ['css/style.css']
+				}
+			}
+		},
+
+		watch: {
+			options: {
+				spawn: false
+			},
+			scripts: {
+				files: ['sass/*.scss'],
+				tasks: ['sass', 'postcss', 'csso'] // пишем какие задачи будем запускать когда какой-либо файл scss изменён. наприемр так ['sass', 'postcss'] и т.д.
+			}
+		}
 	});
-	
+
 	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-postcss');
+	grunt.loadNpmTasks('grunt-csso');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	// Есть плагин котрый делает это автоматически 	
-	
+	// Есть плагин котрый делает это автоматически
+
 	grunt.registerTask('default', ['sass', 'watch']);
 };
 
